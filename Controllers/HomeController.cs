195d7edbc6da4e71705fac9efe7914f9690fafe4
Douglas Regehr <regehrdoug@gmail.com>
2023-03-01 22:38:43 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Amazoon.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Amazoon.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IBookStoreRepository repo;
+
+        public HomeController (IBookStoreRepository temp)
+        {
+            repo = temp;
+        }
+        public IActionResult Index()
+        {
+            var x = repo.Books.ToList();
+
+            return View(x);
+        }
+    }
+}
---
+++ Models/EFBookstoreRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Amazoon.Models
+{
+    public class EFBookstoreRepository : IBookStoreRepository
+    {
+        private BookstoreContext context { get; set; }
+        
+        public EFBookstoreRepository (BookstoreContext temp)
+        {
+            context = temp;
+        }
+
+        public IQueryable<Book> Books => context.Books;
+    }
+}
